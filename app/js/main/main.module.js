5e3e6a1050e50d68b3d52d8981c243a82662c7ff
andriyarterchuk <andriyarterchuk@gmail.com>
2017-04-14 00:52:53 +0300

    LVRUBYM-242:added component

--- app/js/main/main.module.js
+++ app/js/main/main.module.js
@@ -5,7 +5,7 @@ var presenceReportModule = require('./../presenceReport/presenceReport.module.js
 var studentModule = require('./../student/student.module.js');
 var groupService = require('./../common/services/group.service.js');
 var staticModule = require('./../static/static.module.js');
-
+var myDayReportModule = require('./../myDayReport/myDayReport.module.js');
 require('../../css/style.scss');
 
 module.exports = angular
@@ -16,7 +16,8 @@ module.exports = angular
     presenceReportModule.name,
     studentModule.name,
     groupService.name,
-    staticModule.name
+    staticModule.name,
+    myDayReportModule.name
   ])
   .config(function($mdThemingProvider) {
     $mdThemingProvider

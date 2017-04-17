c1277f2601881303dd01af6f108129014ab59b51
Vitalia <vitalia.durnyak@gmail.com>
2017-04-17 11:53:53 +0300

    LVRUBYM-221:Add component for bottle report

--- app/js/common/resources/bottle.resource.js
+++ app/js/common/resources/bottle.resource.js
@@ -1,5 +1,6 @@
 var bottleCon = require('./../constants.js');
 module.exports = angular
+<<<<<<< 7e2b78612584ecdd869040d8426b59d05d3d472d
 .module('bottle.resource', [])
 .factory('bottleResource', bottleResource);
 
@@ -14,3 +15,22 @@ function bottleResource($resource, globalSettings) {
       'update': {method: 'PUT'}
     });
 }
+=======
+  .module('bottle.resource', [
+    bottleCon.name
+  ])
+  .factory('bottleResource', bottleResource)
+
+
+bottleResource.$inject = ['$resource', 'api'];
+
+function bottleResource($resource, api) {
+  return $resource(api.URL + '/groups/21/bottle_reports/:bottle_report_id/bottles/:id.json',
+  	{ id: '@id', bottle_report_id: '@bottle_report_id'},
+  {
+       'update': { method:'PUT' }
+   });
+}
+
+
+>>>>>>> LVRUBYM-221:Add component for bottle report

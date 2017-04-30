629f61f6f4d22397a258e524aa59160f8b337e20
Mykhailo Shtohryn <mshtogryn@gmail.com>
2017-04-30 22:28:08 +0300

    LVRUBYM-191: Added style.scss

--- app/js/common/resources/reportTime.resource.js
+++ app/js/common/resources/reportTime.resource.js
@@ -8,7 +8,8 @@ function reportTimeResource($resource, globalSettings, currentGroupDay) {
   return $resource(globalSettings.SERVER_URL_V1 + '/report_times/:id.json',
     {id: '@id',
     presence_report_id: '@presence_report_id',
-    presence_report: {group_id: currentGroupDay.group_id}},
+    presence_report: {group_id: currentGroupDay.group_id,
+                      day: currentGroupDay.day}},
     {
       'update': {method: 'PUT'}
     });

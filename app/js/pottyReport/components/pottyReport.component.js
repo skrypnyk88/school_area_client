610375721079b62b27303a247192ab3923c375bc
hmaherovska <maher.olena96@gmail.com>
2017-04-30 22:07:08 +0300

    LVRUBYM-243: fixed js with SublimeLinter-jscs

--- app/js/pottyReport/components/pottyReport.component.js
+++ app/js/pottyReport/components/pottyReport.component.js
@@ -1,10 +1,10 @@
 module.exports = angular
-	.module('pottyReport.component',[])
-	.component('pottyComponent',{
-		templateUrl: './app/js/pottyReport/components/pottyReport.template.html',
-		controller: PottyReportController
-	});
+.module('pottyReport.component', [])
+.component('pottyComponent', {
+  templateUrl: './app/js/pottyReport/components/pottyReport.template.html',
+  controller: PottyReportController
+});
 
-	function PottyReportController(){
-		var ctrl = this;
-	}
+function PottyReportController() {
+  var ctrl = this;
+}

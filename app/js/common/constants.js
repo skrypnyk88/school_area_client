e0e0ebd5c4d510828fd31c6c5dec454b777ba88a
hmaherovska <maher.olena96@gmail.com>
2017-05-08 16:30:44 +0300

    LVRUBYM-344: Create main template style

--- app/js/common/constants.js
+++ app/js/common/constants.js
@@ -9,4 +9,25 @@ module.exports = angular
   .constant('errorMessages', {
     NO_AUTH: 'Invalid Email/Password',
     AUTH_TIMEOUT: 'Session has expired'
-  });
+  })
+  .constant('menuItems', [
+      {name: 'Teacher report',  link: '#', submenu: [
+        {name: 'Presence',          link: 'main.presenceReport'},
+        {name: 'Diet',              link: '#'},
+        {name: 'Bottle',            link: '#'},
+        {name: 'Meals',             link: '#'},
+        {name: 'Potty',             link: '#'},
+        {name: 'Nap',               link: '#'},
+        {name: 'Rewards',           link: '#'},
+        {name: 'Personal notes',    link: '#'},
+        {name: 'Class notes',       link: '#'}
+      ]},
+      {name: 'Parens Sharing',  link: '#'},
+      {name: 'Messages',        link: '#'},
+      {name: 'Notifications',   link: '#'},
+      {name: 'Calendar',        link: '#'},
+      {name: 'Photo Gallery',   link: '#'},
+      {name: 'Rewards',         link: '#'},
+      {name: 'Contacts',        link: '#'}
+    ]
+  );

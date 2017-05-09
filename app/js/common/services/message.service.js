d17268a4b9dfca9a4a7a367028fde718de6cc57a
Julia Shtohryn <julia.ilkiv@gmail.com>
2017-05-10 00:31:53 +0300

    LVRUBYM-352: Added Message service

---
+++ app/js/common/services/message.service.js
@@ -0,0 +1,30 @@
+module.exports = angular
+.module('message.service', [
+  ])
+.factory('messageService', messageService);
+
+messageService.$inject = ['$mdToast'];
+
+function messageService($mdToast) {
+  var service = {
+    toggleMsg: toggleMsg
+  };
+  return service;
+
+  function toggleMsg(response) {
+    if (Array.isArray(response.data)) {
+      var msgs = response.data.join('; ');
+    } else {
+      var msgs = response.data;
+    }
+
+    var toast = $mdToast.simple()
+      .textContent(msgs)
+      .action('OK')
+      .highlightAction(false)
+      .position('top right')
+      .hideDelay(9000);
+
+    $mdToast.show(toast);
+  }
+};

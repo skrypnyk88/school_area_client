84e0fcc266847b89c0aea5cdeb271beadaaeb1ce
hmaherovska <maher.olena96@gmail.com>
2017-05-10 20:29:50 +0300

    LVRUBYM-349:Fixed component

--- app/js/main/components/main.component.js
+++ app/js/main/components/main.component.js
@@ -24,35 +24,19 @@ function MainController($scope, $state, auth, currentGroupDay, $translate) {
   var ctrl = this;
 
   ctrl.currentGroupDay = currentGroupDay;
+<<<<<<< e10be0b01dcf0aed273bbfa06318df88200c0060
 <<<<<<< 4ac0622ae5f72bcd5cc1cb348264d9c657b9d123
 =======
   ctrl.menuItems = menuItems;
   ctrl.activeMenu = localStorage.getItem('activeMenu') || 0;
   ctrl.activeSubmenu = localStorage.getItem('activeSubmenu') || 0;
   ctrl.submenuOpened = false;
+=======
+>>>>>>> LVRUBYM-349:Fixed component
   ctrl.selectedDate = new Date();
   ctrl.monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   ctrl.dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-  ctrl.setActiveMenu = function(index) {
-    ctrl.activeMenu = index;
-    localStorage.setItem('activeMenu', index);
-
-    //if Teacher report is not chosen
-    if (index == 0) {
-      ctrl.submenuOpened = !ctrl.submenuOpened;
-    } else {
-      ctrl.activeSubmenu = -1;
-      ctrl.submenuOpened = false;
-    }
-  };
-
-  ctrl.setActiveSubmenu = function(index) {
-    ctrl.activeMenu = 0;
-    ctrl.activeSubmenu = index;
-    localStorage.setItem('activeSubmenu', index);
-  };
 
   ctrl.nextDate = function() {
     ctrl.selectedDate.setDate(ctrl.selectedDate.getDate() + 1);

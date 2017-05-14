022901b755f19098ee69b3374058c5af604864ed
Mykhailo Shtohryn <mshtogryn@gmail.com>
2017-05-14 15:56:30 +0300

    routes

--- app/js/presenceReport/components/presenceReport.component.js
+++ app/js/presenceReport/components/presenceReport.component.js
@@ -1,5 +1,4 @@
 var presenceReportServ = require('./../../common/services/presenceReport.service');
-
 module.exports = angular
   .module('presenceReport.component', [
     presenceReportServ.name
@@ -9,11 +8,15 @@ module.exports = angular
     controller: PresenceReportController
   });
 
-PresenceReportController.$inject = ['presenceReportService','toggleMessage']
+PresenceReportController.$inject = ['presenceReportService','toggleMessage'];
+function PresenceReportController(presenceReportService, toggleMessage) {
 
-function PresenceReportController(presenceReportService,toggleMessage) {
   var ctrl = this;
-  var regex = /^[\d\- ]{2}:[\d\- ]{2}$/;
+  var timeRegex = /^[\d\- ]{2}:[\d\- ]{2}$/;
+  var validTime = {hour: '2-digit',
+                  minute: '2-digit',
+                  hour12: false
+                };
 
   ctrl.loadPresenceReports = function() {
     presenceReportService.getPresenceReports().then(
@@ -24,9 +27,7 @@ function PresenceReportController(presenceReportService,toggleMessage) {
 
   ctrl.addReportTime = function(presenceReport) {
     if (presenceReport.report_time == false || presenceReport.report_time.slice(-1)[0].end_time) {
-      presenceReport.start_time = new Date().toLocaleTimeString([], {hour: '2-digit',
-                                                                      minute: '2-digit',
-                                                                      hour12: false});
+      presenceReport.start_time = new Date().toLocaleTimeString([], validTime);
       presenceReportService.addReportTime(presenceReport)
                        .then(function(reportTime) {
           presenceReport.report_time.push(reportTime);
@@ -36,26 +37,32 @@ function PresenceReportController(presenceReportService,toggleMessage) {
   };
 
   ctrl.updateReportTime = function(reportTime, presenceReport) {
-
-    if ((reportTime.end_time != null) &&
-         (reportTime.start_time < reportTime.end_time) &&
-         (regex.test(reportTime.start_time)) &&
-         (regex.test(reportTime.end_time))) {
-      presenceReportService.updateReportTime(reportTime, presenceReport).then(function(reportTime) {
-        return reportTime;
-      });
+    if (reportTime.end_time != null) {
+      if (reportTime.start_time < reportTime.end_time) {
+        if ((timeRegex.test(reportTime.start_time)) &&
+           (timeRegex.test(reportTime.end_time))) {
+          presenceReportService.updateReportTime(reportTime, presenceReport)
+            .then(function(reportTime) {
+            return reportTime;
+          });
+        } else {
+          ctrl.loadPresenceReports();
+          toggleMessage.showMessages(['Problem with validation']);
+        };
+      } else {
+        ctrl.loadPresenceReports();
+        toggleMessage.showMessages(['Start time must be earlier than end time']);
+      };
     } else {
       ctrl.loadPresenceReports();
-      toggleMessage.showMessages(["Problem with validation"])
-    };
+      toggleMessage.showMessages(['Can edit after enter end time']);
+    }
   };
 
   ctrl.updateEndTime = function(presenceReport) {
     if (presenceReport.report_time.slice(-1)[0].end_time == null) {
       reportTime = presenceReport.report_time.slice(-1)[0];
-      reportTime.end_time = new Date().toLocaleTimeString([], {hour: '2-digit',
-                                                                minute: '2-digit',
-                                                                hour12: false});
+      reportTime.end_time = new Date().toLocaleTimeString([], validTime);
       presenceReportService.updateEndTime(reportTime, presenceReport).then(function(reportTime) {
         return reportTime;
       });
